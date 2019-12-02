7a71e72925e6c647aa04832dfba8ac221c2a3ea0
NTN9115 <accelerator314@gmail.com>
2019-12-02 16:36:31 +0800

    fix bug

---
+++ src/main/java/space/nyuki/questionnaire/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package space.nyuki.questionnaire.config;
+
+/**
+ * @author ning
+ * @createTime 12/2/19 2:25 PM
+ * @description
+ */
+public class RedisConfig {
+}
---
+++ src/main/java/space/nyuki/questionnaire/pojo/ResultCollection.java
@@ -0,0 +1,9 @@
+package space.nyuki.questionnaire.pojo;
+
+/**
+ * @author ning
+ * @createTime 12/2/19 11:28 AM
+ * @description 调查结果表
+ */
+public class Findings {
+}
